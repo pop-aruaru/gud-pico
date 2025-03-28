b70f3d90ea1a81cf373da0a19f4cb18de2202d09
Aruaru <4726806+pop-aruaru@users.noreply.github.com>
2025-03-28 23:58:49 +0900

    Create camera2fb.py

---
+++ camera2fb.py
@@ -0,0 +1,27 @@
+import picamera2
+import cv2
+from picamera2 import Picamera2
+from IPython.display import display, Image
+
+camera = Picamera2()
+video_config=camera.create_video_configuration()
+transform=video_config['transform']
+transform.vflip=True 
+transform.hflip=True
+camera.configure(video_config)
+camera.start() # capture開始
+img=camera.capture_array()
+qvga=cv2.resize(img,(480,320))
+qvga3=cv2.cvtColor(qvga,cv2.COLOR_BGR2RGBA)#色がおかしい 肌色が紫になるので。
+with open('/dev/fb0', 'wb') as f:
+    for i in range(0,1000):
+        img=camera.capture_array()
+        qvga=cv2.resize(img,(480,320))
+        qvga3=cv2.cvtColor(qvga,cv2.COLOR_BGR2RGBA)#色がおかしい 肌色が紫になるので。
+        # デバイスファイルへの書き込み
+        f.write(qvga3)
+        f.seek(0, os.SEEK_SET)
+
+camera.stop()
+camera.close()
+print('end')
